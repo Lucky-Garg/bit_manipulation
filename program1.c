8f270d33dc1fe75b091b5b8d49580bd34b64e7a2
lucky garg <lucky.work.005@gmail.com>
2025-01-06 23:45:32 +0530

    first programme

---
+++ program1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int count_set_bits(int num)
+{
+    int count = 0;
+    int temp;
+    while (num != 0)
+    {
+        temp = num & 1;
+        if (temp == 1)
+            count += 1;
+        num = num >> 1;
+    }
+    return count;
+}
+void main()
+{
+    int num;
+    printf("Enter number :");
+    scanf("%d", &num);
+    int res;
+    res = count_set_bits(num);
+    printf("\nNumber of set bits are :");
+    printf("%d", res);
+}
