abd2c921721b16c33f908ceb7e71c63fe75ab472
lucky garg <lucky.work.005@gmail.com>
2025-01-07 00:13:58 +0530

    seventh program

---
+++ assignment1/program7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int bit_square(int num)
+{
+    int result = 0;
+    int temp = num;
+    while (temp > 0)
+    {
+        if (temp & 1)
+        {
+            result += num;
+        }
+        num <<= 1;
+        temp >>= 1;
+    }
+    return result;
+}
+void main()
+{
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    int res = bit_square(num);
+    printf("Square of %d is: %d\n", num, res);
+}
