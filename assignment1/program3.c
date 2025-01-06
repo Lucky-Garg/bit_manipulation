888a465c38bce9b3a095e3421b710193df675bc3
lucky garg <lucky.work.005@gmail.com>
2025-01-07 00:06:49 +0530

    third program

---
+++ assignment1/program3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int power_of_two(int n)
+{
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
+void main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (power_of_two(num))
+    {
+        printf("%d is a power of two.\n", num);
+    }
+    else
+    {
+        printf("%d is not a power of two.\n", num);
+    }
+}
