ecff95f8c6d77bceed26280ebdb6b6d6a3b49afa
lucky garg <lucky.work.005@gmail.com>
2025-01-07 00:10:36 +0530

    fifth program

---
+++ assignment1/program5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int bits_flipped(int num, int a, int b)
+{
+    num = a ^ b;
+    int count = 0;
+    int temp;
+    while (num != 0)
+    {
+        temp = num & 1;
+        if (temp == 1)
+            count += 1;
+        num = num >> 1;
+    }
+    return count;
+}
+void main()
+{
+    int a, b, num;
+    printf("Enter the numbers :");
+    scanf("%d %d", &a, &b);
+    int res;
+    res = bits_flipped(num, a, b);
+    printf("Numebr of bits to be flipped:");
+    printf("%d", res);
+}
